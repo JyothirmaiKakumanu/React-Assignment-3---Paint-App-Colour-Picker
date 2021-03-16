6552056d44dfc3c002daffe0c86c6513eb926b3f
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-03-16 17:52:12 +0530

    commited

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,13 +1,17 @@
 import React,{useState} from 'react';
+import '../styles/Child.css';
 
 const Selection = ({applyColor}) => {
     let [selectionBG, setSelectionBG] = useState({ background: '' })
 
+    // const changeColor = (nextBackground) => {
+    //     setSelectionBG({ background: props.nextBackground });
+    //   };
    
     return (
     
             <div className="fix-box" style={selectionBG} onClick={()=>{
-                applyColor(setSelectionBG)
+               applyColor(setSelectionBG)
             }}>
                 <h2 className="subheading">Selection</h2>
             </div>
